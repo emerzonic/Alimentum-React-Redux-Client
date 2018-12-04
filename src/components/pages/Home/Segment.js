67ae0ddb441cbd744fc3c11586cf7824210b7f9d
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-04 01:45:02 -0600

    added home page

--- src/components/pages/Home/Segment.js
+++ src/components/pages/Home/Segment.js
@@ -1,58 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import  {Component} from 'react';
 import './home.css';
 
-const Segment = () => (
-<div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-<div class="card bg-dark text-white col-md-6">
-  <img class="card-img" src=".../100px270/#55595c:#373a3c/text:Card image" alt="Category"/>
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
-  </div>
-</div>
-</div>
-);
 
+class Segment extends Component {
+  render() {
+    let Segments = this.props.list.map((item, i) => {
+      return <div className="card col-md-4 my-2 border-0 shadow-sm" key={i}>
+                <img className="card-img-top" src={item.img} alt={this.props.name}/>
+                <div className="card-body">
+                  <h5 className="card-title">{item.cat}</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="google" className="btn btn-primary col-sm-12">Go somewhere</a>
+                </div>
+              </div>
+        });
+    return (
+      Segments
+    );
+  }
+}
+ 
 export default Segment;
