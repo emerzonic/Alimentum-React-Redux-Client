e4c8aef4c7ad15727384a6cc159fc99d6a588f67
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-04 01:44:36 -0600

    added home page

--- src/assets.js
+++ src/assets.js
@@ -1,18 +1,17 @@
 //Images object required from the "img" folder
 const imagesObj = {
-    img1: require('./images/beeg.jpg'),
-    img2: require('../images/breakfast.jpeg'),
-    img3: require('../images/chicken.jpeg'),
-    img4: require('../images/desert.jpeg'),
-    img5: require('../images/lamb.jpeg'),
-    img6: require('../images/misc.jpeg'),
-    img7: require('../images/pasta.jpeg'),
-    img8: require('../images/pork.jpeg'),
-    img9: require('../images/seafood.jpeg'),
-    img10: require('../images/side.jpeg'),
-    img11: require('../images/starter.jpeg'),
-    img12: require('../images/vegan.jpeg'),
-    img13: require('../images/vege.jpeg'),
+    img1: require('./images/vege.jpg'),
+    img2: require('./images/breakfast.jpg'),
+    img3: require('./images/chicken.jpg'),
+    img4: require('./images/desert.jpg'),
+    img5: require('./images/lamb.jpg'),
+    img6: require('./images/pasta.jpg'),
+    img7: require('./images/pork.jpg'),
+    img8: require('./images/seafood.jpg'),
+    img9: require('./images/side.jpg'),
+    img10: require('./images/starter.jpg'),
+    img11: require('./images/vegan.jpg'),
+    img12: require('./images/beef.jpg'),
   }
 
   export default imagesObj;
