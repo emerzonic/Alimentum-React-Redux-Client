8a846ff1dd8e7b5b120f352c7e9b0c17c9a555ca
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-04 01:44:42 -0600

    added home page

---
+++ src/category.js
@@ -0,0 +1,17 @@
+ const category = {
+    "cat1":"Vegetarian",
+    "cat2":"Breakfast",
+    "cat3":"Chicken",
+    "cat4":"Desert",
+    "cat5":"Lamb",
+    "cat6":"Pasta",
+    "cat7":"Pork",
+    "cat8":"Seafood",
+    "cat9":"Side",
+    "cat10":"Starter",
+    "cat11":"Vegan",
+    "cat12":"Beef"
+};
+
+export default category;
+
