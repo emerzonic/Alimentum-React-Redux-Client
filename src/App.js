1ed65658ce92c5083ac41c9e3304c3a025bd41db
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-06 16:45:19 -0600

    refactored react router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import User from "./components/pages/Home/User";
-import Search from "./components/pages/Search/Search";
-import Saved from "./components/pages/Saved/Saved";
+// import Search from "./components/pages/Search/Search";
+// import Saved from "./components/pages/Saved/Saved";
+import RecipeCategories from "./components/pages/Home/RecipeCategory";
+import Recipes from "./components/pages/Home/Recipes";
+import RecipeDetail from "./components/pages/Home/RecipeDetail";
 
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route exact path="/search" component={Search}/>
-      <Route exact path="/saved" component={Saved}/>
+      <Route exact path="/categories" component={RecipeCategories}/>
+      <Route exact path="/categories/recipres" component={Recipes}/>
+      <Route exact path="/categories/recipres/details" component={RecipeDetail}/>
       <Route exact path="/signin" component={User}/>
       <Route exact path="/signup" component={User}/>
       <Route exact path="/signout" component={User}/>
