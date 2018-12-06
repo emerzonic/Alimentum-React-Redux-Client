3d1e88f397a5d0adccaa3c4481d4d14d131fb515
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-06 16:46:52 -0600

    add recipe categories

---
+++ src/components/pages/Home/RecipeCategory.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import  {Component} from 'react';
+import './home.css';
+
+
+class RecipeCategories extends Component {
+  render() {
+    let recipeCategories = this.props.state.categories.map((item, i) => {
+      return <div className="card col-md-4 my-2 border-0 shadow-sm category-div" key={i}>
+                <img className="card-img-top" src={item.img} alt={this.props.name}/>
+                <div className="card-body">
+                  <h5 className="card-title">{item.cat}</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <button className="btn btn-primary col-sm-12" data-type="category" data-param={item.cat} onClick={this.props.onClick}>Go somewhere</button>
+                </div>
+              </div>
+        });
+    return (
+      recipeCategories
+    );
+  }
+}
+ 
+export default RecipeCategories;
