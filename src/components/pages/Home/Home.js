5b77b3d3dfedfbc63a2acb05a57ff4ac5ac803b2
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-06 16:46:21 -0600

    imports

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Component } from 'react';
 import Nav from "./Nav";
-import Segment from "./Segment";
+import RecipeCategories from "./RecipeCategory";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
 import Footer from "./Footer";
 import './home.css';
 import Img from '../../../assets';
 import category from '../../../category';
+import axios from 'axios';
+import util from '../../util';
 class  Home extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            data:[],
+            categories:[],
+            recipes:[],
+            recipeDetail:{},
+            pageTitle:""
         }
 
          this.dataList = [];
@@ -18,42 +25,97 @@ class  Home extends Component {
          this.category = [];
         
          this.sumList = ()=>{
-         for(let i = 0; i<this.images.length && i<this.category.length; i++){
+         for(let i = 0; i<this.images.length; i++){
              const newObj = {
                  img: this.images[i],
                  cat:this.category[i]
              }
-             console.log(newObj)
              this.dataList.push(newObj)
          }
          }
          
-
-
          //This method  takes the "Img" object above extra and 
          //push all the values to the images property array 
          this.getData = (obj, arr) => {
            for (const key of Object.keys(obj)) {
              arr.push(obj[key]);
            }
-         }
+         } 
 
+         this.getDetailData = (obj)=>{
+
+         }
 
     //Get the images once
      this.getData(Img, this.images);
      this.getData(category, this.category);
      this.sumList();
-     this.state.data=this.dataList;
-    }
+     this.state.categories = this.dataList;
+     this.state.pageTitle = "Categories";
+
+    this.onClick = (event) => {
+        let type = event.target.getAttribute("data-type");
+        let param = event.target.getAttribute("data-param");
+        type === "category"?
+        this.getRecipesByCategory(param):
+        type === "name"?
+        this.getRecipesById(param):
+        console.log("no param");
+    };
+    
+
+    this.getRecipesByCategory = (param) =>{  
+        axios.get(`http://localhost:5000/searchByCategory/${param}`).then(res => 
+            this.setState({
+                recipes: res.data,
+                pageTitle:param,
+                categories:[]
+            })
+        ).catch(err => console.log(err));
+    };
+
+    this.getRecipesById = (param) =>{  
+        axios.get(`http://localhost:5000/searchByRecipeId/${param}`).then(res => {
+        let refinedRecipe = util.getRecipeObj(res.data[0]);
+        console.log(refinedRecipe);
+            this.setState({
+                recipes: [],
+                pageTitle:'',
+                categories:[],
+                recipeDetail:refinedRecipe
+            })
+        }).catch(err => console.log(err));
+    };
+}
     
     render() { 
         console.log(this.state);
         return (
             <div>
                 <Nav/>
+                    <div className="jumbotron jumbotron-fluid shadow">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <h1 className="display-4">{this.state.pageTitle}</h1>
+                                </div>
+                                <div className="col-md-8">
+                                    <form className="form-inline mt-3 w-100">
+                                        <input className="form-control mr-sm-2" type="search" placeholder="Search Recipes" aria-label="Search"/>
+                                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="container">
                         <div className="row">
-                        <Segment list={this.state.data}/>
+                        {this.state.categories.length?
+                        <RecipeCategories state={this.state} onClick={this.onClick}/>:
+                        this.state.recipes.length?
+                        <Recipes state={this.state} onClick={this.onClick}/>:
+                        this.state.recipesDetail!==null?
+                        <RecipeDetail recipeDetail={this.state.recipeDetail} onClick={this.onClick}/>:""}
                         </div>
                     </div>
                 <Footer/>
