702a737ace8c7e48ab19bc1ebf11ac78426bee3a
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-09 14:39:11 -0600

    added save button

--- src/components/pages/Home/RecipeDetail.js
+++ src/components/pages/Home/RecipeDetail.js
@@ -16,26 +16,29 @@ class RecipeDetail extends Component {
                           <img className="card-img-top img-fluid rounded ml-md-4" src={this.props.recipeDetail.strMealThumb} alt={this.props.name}/>
                       </div>
                       <div className="col-md-4">
-                            <p className="card-text"><small className="text-muted"></small></p>
-                            <button className="btn btn-primary mx-1" data-type="name" onClick={this.props.onClick}><i className="fab fa-youtube display-4"></i></button>
-                            <button className="btn btn-primary mx-1" data-type="name" onClick={this.props.onClick}><i className="fas fa-save display-4"></i></button>
-                            <button className="btn btn-primary mx-1" data-type="name" onClick={this.props.onClick}><i className="fas fa-external-link display-4"></i></button>
+                            <button className="btn btn-primary mx-1 w-100 m-2 " data-type="name" onClick={this.props.onClick}>Watch Video</button>
+                            <button className="btn btn-primary mx-1 w-100 m-2" data-type="name" onClick={this.props.onClick}>Save Recipe</button>
+                            <button className="btn btn-primary mx-1 w-100 m-2" data-type="name" onClick={this.props.onClick}>Read More</button>
                       </div>
                   </div>
                   <div className="card-body">
                       <div className="row">
+                          <div className="col-md-12">
+                              <h5 className="card-title display-4">{this.props.recipeDetail.strMeal}</h5>
+                          </div>
+                      </div>
+                      <div className="row">
                           <div className="col-md-8">
-                              <h5 className="card-title">{this.props.recipeDetail.strMeal}</h5>
-                              <p>Instructions</p>
-                              <ul className="card-text bg-light p-4">
+                          <h5 className="card-title">Instructions</h5>
+                          <ul className="card-text bg-light p-4">
                                   {this.props.recipeDetail.strInstructions.map((instr, i)=>{
                                     return <li className="" key={i}>{instr}</li>
                                   })}
                               </ul>
                           </div>
                         <div className="col-md-4">
+                        <h5 className="card-title">Ingredients</h5>
                           <div className="card w-100 mr-2">
-                              <p>Ingredients</p>
                               <ul className="list-group list-group-flush border-0">
                                 {this.props.recipeDetail.strIngredients.map((ing, i)=>{
                                   return <li className="list-group-item py-1" key={i}>{this.props.recipeDetail.strMeasurements[i]} {ing}</li>
