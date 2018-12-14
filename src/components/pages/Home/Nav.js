fb754fbd237169438276efeb2f5da771a675f87b
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-14 08:53:50 -0600

    refactor navbar

--- src/components/pages/Home/Nav.js
+++ src/components/pages/Home/Nav.js
@@ -5,7 +5,10 @@ const Nav = () => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <a href="/" className="navbar-brand">ALIMENTUM</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                        data-target="#navbarNavDropdown" 
+                        aria-controls="navbarNavDropdown" 
+                        aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     <span className="">Menu</span>
                 </button>
@@ -16,8 +19,8 @@ const Nav = () => (
                         </li>
                         <li className="nav-item dropdown  justify-self-end">
                             <div className="d-flex flex-column flex-md-row justify-content-end" aria-labelledby="navbarDropdownMenuLink">
-                                <a href="/" className="nav-link">Login</a>
-                                <a href="/" className="nav-link">Sign Up</a>
+                                <a href="/login" className="nav-link">Login</a>
+                                <a href="/login" className="nav-link">Sign Up</a>
                                 <a href="/" className="nav-link">Logout</a>
                             </div>
                         </li>
