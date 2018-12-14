af2cc7a4fb53072a34354aa2f2c175640836319b
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-14 08:52:00 -0600

    moved category to separate file

--- src/components/pages/Home/Category.js
+++ src/components/pages/Home/Category.js
@@ -1,120 +1,138 @@
-// import React from 'react';
-// import { Component } from 'react';
-// import Nav from "./Nav";
-// import RecipeCategories from "./RecipeCategory";
-// import Recipes from "./Recipes";
-// import RecipeDetail from "./RecipeDetail";
-// import Footer from "./Footer";
-// import './home.css';
-// import Img from '../../../assets';
-// import category from '../../../category';
-// import axios from 'axios';
-// class  Category extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { 
-//             categories:[],
-//             recipes:[],
-//             recipeDetail:{},
-//             pageTile:''
-//         }
+import React from 'react';
+import { Component } from 'react';
+import RecipeCategories from "./RecipeCategory";
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import Header from "./Header";
+import './home.css';
+import assets from '../../../assets';
+import axios from 'axios';
+import util from '../../util';
 
-//          this.dataList = [];
-//          this.images = [];
-//          this.category = [];
-        
-//          this.sumList = ()=>{
-//          for(let i = 0; i<this.images.length; i++){
-//              const newObj = {
-//                  img: this.images[i],
-//                  cat:this.category[i]
-//              }
-//              this.dataList.push(newObj)
-//          }
-//          }
+
+class  Category extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            categories:[],
+            recipes:[],
+            recipeDetail:{},
+            pageTitle:"",
+            saveFeedBack:"",
+            unsaveFeedBack:"",
+            name:""
+        }
+
+        //  this.dataList = [];
+        //  this.images = [];
+         this.categories = [];
          
-//          //This method  takes the "Img" object above extra and 
-//          //push all the values to the images property array 
-//          this.getData = (obj, arr) => {
-//            for (const key of Object.keys(obj)) {
-//              arr.push(obj[key]);
-//            }
-//          } 
-//         //Get the images once
-//         this.getData(Img, this.images);
-//         this.getData(category, this.category);
-//         this.sumList();
-//         this.state.categories = this.dataList;
-//         this.state.pageTile = "Categories";
+         //This method  takes the "Img" object above extra and 
+         //push all the values to the images property array 
+         this.getData = (obj, arr) => {
+           for (const key of Object.keys(obj)) {
+               console.log(obj[key])
+             arr.push(obj[key]);
+           }
+         } 
 
-//         this.onClick = (event) => {
-//             let type = event.target.getAttribute("data-type");
-//             let param = event.target.getAttribute("data-param");
-//             type === "category"?
-//             this.getRecipesByCategory(param):
-//             type === "name"?
-//             this.getRecipesById(param):
-//             console.log("no param");
-//     };
+         this.getDetailData = (obj)=>{
+
+         }
+
+    //Get the images once
+     this.getData(assets.category, this.categories);
+     this.state.categories = this.categories;
+     this.state.pageTitle = "Categories";
+
+    this.onClick = (event) => {
+        let type = event.target.getAttribute("data-type");
+        let param = event.target.getAttribute("data-param");
+        type === "category"?
+        this.getRecipesByCategory(param):
+        type === "name"?
+        this.getRecipesById(param):
+        type === "save"?
+        this.saveRecipe():
+        console.log("no param");
+    };
+
+    this.onChange = (e) => {
+        this.setState({name:e.target.value})
+    };
+
+    this.onSubmit = (e) => {
+        e.preventDefault();
+        let name = this.state.name;
+        axios.get(`http://localhost:5000/searchByName/${name}`).then(res => 
+        this.setState({
+            recipes: res.data,
+            pageTitle:"Results for " + name,
+            categories:[]
+        })
+    ).catch(err => console.log(err));
+    };
     
 
-//     this.getRecipesByCategory = (param) =>{  
-//         axios.get(`http://localhost:5000/searchByCategory/${param}`).then(res => 
-//             this.setState({
-//                 recipes: res.data,
-//                 pageTile:param,
-//                 categories:[]
-//             })
-//         ).catch(err => console.log(err));
-//     };
+    this.getRecipesByCategory = (param) =>{  
+        axios.get(`http://localhost:5000/searchByCategory/${param}`).then(res => 
+            this.setState({
+                recipes: res.data,
+                pageTitle:param,
+                categories:[]
+            })
+        ).catch(err => console.log(err));
+    };
 
-//     this.getRecipesById = (param) =>{  
-//         axios.get(`http://localhost:5000/searchByRecipeId/${param}`).then(res => 
-//             this.setState({
-//                 recipes: [],
-//                 pageTile:res.data.strMeal,
-//                 categories:[],
-//                 recipeDetail:res.data[0]
-//             })
-//         ).catch(err => console.log(err));
-//     };
+    this.getRecipesById = (param) =>{  
+        axios.get(`http://localhost:5000/searchByRecipeId/${param}`).then(res => {
+        let refinedRecipe = util.getRecipeObj(res.data[0]);
+            this.setState({
+                recipes: [],
+                pageTitle:'',
+                categories:[],
+                recipeDetail:refinedRecipe
+            })
+        }).catch(err => console.log(err));
+    };
 
-// }
-    
-//     render() { 
-//         console.log(this.state);
-//         return (
-//             <div>
-//                 <Nav/>
-//                     <div className="jumbotron jumbotron-fluid shadow">
-//                         <div className="container">
-//                             <div className="row">
-//                                 <div className="col-md-6">
-//                                     <h1 className="display-4">{this.state.pageTile}</h1>
-//                                 </div>
-//                                 <div className="col-md-6">
-//                                     <form className="form-inline mt-3">
-//                                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-//                                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//                                     </form>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className="container">
-//                         <div className="row">
-//                         {this.state.categories.length?
-//                         <RecipeCategories state={this.state} onClick={this.onClick}/>:
-//                         this.state.recipes.length?
-//                         <Recipes state={this.state} onClick={this.onClick}/>:
-//                         this.state.recipesDetail!==null?
-//                         <RecipeDetail recipeDetail={this.state.recipeDetail} onClick={this.onClick}/>:""}
-//                         </div>
-//                     </div>
-//                 <Footer/>
-//             </div>
-//          );
-//     }
-// }
+    this.saveRecipe = () =>{  
+        let recipe = this.state.recipeDetail;
+        let username = "emerson";
+        axios.post(`http://localhost:5000/saveRecipe/${username}`, recipe).then(res => {
+            let save = "";
+            let unsave = "";
+            if(res.data === "success"){
+                save = "Your recipe was successfully saved!"
+            }else{
+                unsave = "This recipe is already saved as favorite."
+            }
+            this.setState({
+                saveFeedBack:save,
+                unsaveFeedBack:unsave
+            })            
+        }).catch(err => console.log(err));
+    };
+}
+    render() { 
+        console.log(this.state);
+        return (
+            <div>
+                <Header state={this.state} onChange={this.onChange} onSubmit={this.onSubmit}/>
+                    <div className="container">
+                        <div className="row">
+                        {this.state.categories.length?
+                            <RecipeCategories state={this.state} onClick={this.onClick}/>:
+                            this.state.recipes.length?
+                                 <Recipes state={this.state} onClick={this.onClick}/>:
+                            this.state.recipesDetail!==null?
+                                <RecipeDetail recipeDetail={this.state.recipeDetail} onClick={this.onClick}/>:""
+                        }
+                        </div>
+                    </div>
+            </div>
+         );
+    }
+}
  
-// export default Category;
+export default Category;
