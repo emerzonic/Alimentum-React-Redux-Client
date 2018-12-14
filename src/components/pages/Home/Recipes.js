0a4e8df689ab253fe544770eef085abd4106cecc
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-14 08:54:48 -0600

    add round images

--- src/components/pages/Home/Recipes.js
+++ src/components/pages/Home/Recipes.js
@@ -7,7 +7,9 @@ class Recipes extends Component {
   render() {
     let recipes = this.props.state.recipes.map(item => {
       return <div className="card col-md-4 my-2 border-0 shadow-sm recipe-div" key={item.idMeal}>
-                <img className="card-img-top rounded-circle"  data-type="name" data-param={item.idMeal} onClick={this.props.onClick} src={item.strMealThumb} alt={this.props.name}/>
+                <img className="card-img-top rounded-circle mt-3"  
+                     data-type="name" data-param={item.idMeal} 
+                     onClick={this.props.onClick} src={item.strMealThumb} alt={this.props.name}/>
                 <div className="card-body recipe-title">
                   <h5 className="card-title text-center">{item.strMeal}</h5>
                 </div>
