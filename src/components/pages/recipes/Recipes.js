e99556c4c078fc3a6c0fa1e5dea433a9497e4c75
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 01:07:08 -0600

    page header links

--- src/components/pages/recipes/Recipes.js
+++ src/components/pages/recipes/Recipes.js
@@ -2,6 +2,9 @@ import React from 'react';
 import  {Component} from 'react';
 import axios from 'axios';
 import '../home/home.css';
+import Header from "../../sections/Header";
+import PageHeader from '../../sections/Page Header';
+
 
 
 class Recipes extends Component {
@@ -18,7 +21,7 @@ this.getRecipesByCategory = () =>{
     axios.get(`http://localhost:5000/searchByCategory/${param}`).then(res => 
         this.setState({
             recipes: res.data,
-            pageTitle:param,
+            pageTitle:param +" Recipes",
         })
     ).catch(err => console.log(err));
   };
@@ -30,21 +33,27 @@ componentDidMount(){
 
 
   render() {
-      return  <div className="container">
-                  <div className="row">
-                    {this.state.recipes.map(item => 
-                      <div className="card col-md-4 my-2 border-0 shadow-sm recipe-div" key={item.idMeal}>
-                        <img className="card-img-top rounded-circle mt-3"  
-                            data-type="name"
-                            src={item.strMealThumb} alt={this.props.name} 
-                            onClick={()=>this.props.history.push(`/categories/${this.state.pageTitle}/${item.idMeal}`)}/>
-                        <div className="card-body recipe-title">
-                          <h5 className="card-title text-center">{item.strMeal}</h5>
-                        </div>
+      return ( 
+        <div>
+            <Header {...this.props} state={this.state}/>
+            <PageHeader {...this.props} state={this.state}/>
+            <div className="container">
+                      <div className="row">
+                        {this.state.recipes.map(item => 
+                          <div className="card col-md-4 my-2 border-0 shadow-sm recipe-div" key={item.idMeal}>
+                            <img className="card-img-top rounded-circle mt-3"  
+                                data-type="name"
+                                src={item.strMealThumb} alt={this.props.name} 
+                                onClick={()=>this.props.history.push(`/categories/${item.strCategory}/${item.idMeal}`)}/>
+                            <div className="card-body recipe-title">
+                              <h5 className="card-title text-center">{item.strMeal}</h5>
+                            </div>
+                          </div>
+                        )}
                       </div>
-                    )}
-                  </div>
-               </div>
+              </div>
+          </div>
+               )
               }
     }
  
