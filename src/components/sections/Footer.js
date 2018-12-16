68c6a3797077ca7049a7a7f4a1b3ea9a4e5f1bd8
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 01:08:26 -0600

    redesign footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -3,7 +3,7 @@ import '../pages/home/home.css';
 
 const Footer = () => (
     <nav className="nav navbar-light text-center">
-        <span className="item p-5">Built with Spring Boot & React Copyright &copy;2018 Emerson Doyah Projects</span>
+        <span className="item text-light p-5">Built with Spring Boot REST Service backend & React JS client. Copyright &copy;2018 Emerson Doyah Projects</span>
     </nav> 
 );
 
