0e6d8d65eb1808e51f2a0c56a9a0fc31b0f0ff35
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 01:02:30 -0600

    clean up

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-
+import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import Nav from "./components/sections/Nav";
 import Home from "./components/pages/home/Home";
-// import User from "./components/pages/user/user";
-// import Search from "./components/pages/Search/Search";
-// import Saved from "./components/pages/Saved/Saved";
-import RecipeCategories from "./components/pages/category/RecipeCategory";
+import Footer from "./components/sections/Footer";
+import UserForm from "./components/pages/user/UserForm";
+import Results from "./components/pages/search/Results";
 import Recipes from "./components/pages/recipes/Recipes";
 import Category from "./components/pages/category/Category";
-import RecipeDetail from "./components/pages/recipeDetail/RecipeDetail";
 import Favorites from "./components/pages/favorites/Favorites";
-import UserForm from "./components/pages/user/UserForm";
-import Nav from "./components/sections/Nav";
-import Footer from "./components/sections/Footer";
-import "./App.css";
-
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import RecipeDetail from "./components/pages/recipeDetail/RecipeDetail";
 
 const App = () => (
   <Router>
     <div>
     <Nav/>
-      <div className="container-fluid .App-container">
+      <div className="container-fluid .App-container p-0 m-0 ">
         <Route exact path="/" component={Home} />
         <Route exact path="/categories" component={Category}/>
         <Route exact path="/categories/:category" component={Recipes}/>
         <Route exact path="/categories/:category/:recipeId" component={RecipeDetail}/>
+        <Route exact path="/search/results" component={Results}/> 
         <Route exact path="/user/favorites" component={Favorites}/>
-        <Route exact path="/user/login" component={UserForm}/>
+        <Route exact path="/user/loginOrSignup" component={UserForm}/>
       </div>
       <Footer/>
     </div>
