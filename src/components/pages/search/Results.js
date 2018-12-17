235f945bb8367c6008ade2219ab8d42f684bcfc9
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-17 11:17:11 -0600

    comment out header from results page

--- src/components/pages/search/Results.js
+++ src/components/pages/search/Results.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import  {Component} from 'react';
 import '../home/home.css';
-import Header from "../../sections/Header";
+// import Header from "../../sections/Header";
 import PageHeader from '../../sections/Page Header';
 import Pusher from '../../sections/Pusher';
+import Loading from '../../sections/Loading';
+
 
 
 
@@ -12,7 +14,9 @@ class Results extends Component {
     super(props);
     this.state = { 
         recipes:[],
-        pageTitle:''
+        pageTitle:'',
+        showBackButton:false,
+        isLoading:true
     }
 
 this.getRecipesByCategory = () =>{ 
@@ -20,8 +24,11 @@ this.getRecipesByCategory = () =>{
     let pageTitle = this.props.location.state.pageTitle; 
         this.setState({
             recipes: recipes,
+            showBackButton:true,
+            isLoading:false,
             pageTitle:recipes?`Showing results for "${pageTitle}".`:
             `There are no recipes found for "${pageTitle}".`
+
         })
     }
 }
@@ -32,10 +39,10 @@ componentDidMount(){
 
   render() {
       return <div>
-                <Header {...this.props} state={this.state}/>
                 <PageHeader {...this.props} state={this.state}/>
                 <div className="container">   
                     <div className="row">
+                    {this.state.isLoading? <Loading/>:""}
                         {this.state.recipes.length? this.state.recipes.map(item => 
                           <div className="card col-md-4 my-2 border-0 shadow-sm recipe-div" key={item.idMeal}>
                             <img className="card-img-top rounded-circle mt-3"  
