d709b6bb363a97e90d4a6d6a1e46eb153ba64cb4
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 21:47:22 -0600

    add login

--- src/components/pages/user/Login.js
+++ src/components/pages/user/Login.js
@@ -1,48 +1,25 @@
 import React from 'react';
-import {
-    Component
-} from 'react';
 import './user.css';
 
-
-class Login extends Component {
-    constructor(props) {
-        super(props);
-    
-    this.handleSubmit = e =>{
-        e.preventDefault();
-        let preciousUser = {
-            username: e.target.username.value,
-            password: e.target.password.value
-        };;
-        if(!preciousUser.username.length ||!preciousUser.password.length){ 
-            return;
-        }else{
-            this.props.handleUserSignin(preciousUser);
-        }
-      }
-}
-
-render() {
-    return (
-        <div className="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
-        <form className="p-md-5 mb-5 shadow-sm border rounded">
+const Login = (props) => (
+    <div className="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+        <form className="p-md-5 mb-5 shadow-sm border rounded" onSubmit={props.onSubmit} request-type="login">
             <h1 className="card-title">Login</h1>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Username</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username"/>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" className="form-control" id="login-username" aria-describedby="username" placeholder="Enter Username"  
+                       name="username" value={props.state.user.username} onChange={props.loginChange}/>
+                <small id="usernameError" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password"/>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="login-password"  placeholder="Enter Password" 
+                        name="password" value={props.state.user.password}  onChange={props.loginChange}/>
+                <small id="passwordError" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-                <button type="submit" class="btn btn-primary">Login</button>
+                <button type="submit" className="btn btn-primary">Login</button>
         </form>
     </div>
-    );
-}
-}
- 
+);
+
 export default Login;
