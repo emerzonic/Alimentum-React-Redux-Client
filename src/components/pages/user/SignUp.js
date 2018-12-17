03db3945a117dd3dc72cd5e418766a4f57d4707e
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 21:47:30 -0600

    add sign up

--- src/components/pages/user/SignUp.js
+++ src/components/pages/user/SignUp.js
@@ -1,52 +1,25 @@
 import React from 'react';
-import {
-    Component
-} from 'react';
+import './user.css';
 
-// import './home.css';
-
-
-class SignUP extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-
-    
-        this.handleSubmit = e =>{
-            e.preventDefault();
-            let newUser = {
-                fullName: e.target.fullName.value,
-                username: e.target.username.value,
-                password: e.target.password.value
-            };
-            if(!newUser.fullName.length ||!newUser.password.length ||!newUser.password.length){ 
-              return;
-          }else{
-              this.props.handleUserSignup(newUser);
-          }
-          }
-    }
-    render() {
-        return (
-            <div className="tab-pane fade" id="signup" role="tabpanel" aria-labelledby="signup-tab">
-            <form className="p-5 mb-5 shadow-sm border rounded">
-                <h1 className="card-title">Sign Up</h1>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Username</label>
-                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Username"/>
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Password"/>
-                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                        <button type="submit" class="btn btn-primary">Sign Up</button>
-            </form> 
-        </div>
-
-        );
-    }
-}
+const SignUP = (props) => (
+    <div className="tab-pane fade" id="signup" role="tabpanel" aria-labelledby="signup-tab">
+            <form className="p-md-5 mb-5 shadow-sm border rounded" onSubmit={props.onSubmit} request-type="signUp">
+            <h1 className="card-title">Sign Up</h1>
+            <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" className="form-control" id="signup-username" aria-describedby="username" placeholder="Enter Username"  
+                    name="username" value={props.state.newUser.username}  onChange={props.signUpChange}/>
+                <small id="usernameError" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div className="form-group">
+                <label htmlFor="exampleInputPassword1">Password</label>
+                <input type="password" className="form-control" id="signup-password"  placeholder="Enter Password" 
+                        name="password" value={props.state.newUser.password} onChange={props.signUpChange}/>
+                <small id="passwordError" className="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+                <button type="submit" className="btn btn-primary">Sign Up</button>
+        </form>
+</div>
+);
 
 export default SignUP;
