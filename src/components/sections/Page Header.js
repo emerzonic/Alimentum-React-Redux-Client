d4c488be32ac041857049d6e87583b092b076f17
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-17 11:17:38 -0600

    refactor

--- src/components/sections/Page Header.js
+++ src/components/sections/Page Header.js
@@ -7,7 +7,8 @@ const PageHeader = (props) => (
              <div className="container">
                 <div className="row">
                     <div className="col-12 mx-auto p-0">
-                         <h1 className="display-4 page-title rounded shadow-sm p-3 m-0">{props.state.pageTitle}
+                    <div className="rounded shadow-sm p-3 m-0 page-title-div">
+                         <h1 className="display-4 page-title mb-0">{props.state.pageTitle}
                             {props.state.saveFeedBack? 
                                 <div className="alert alert-success save-feedback d-md-inline ml-md-5" role="alert">
                                 <i className="fas fa-check mr-2"></i>
@@ -20,10 +21,11 @@ const PageHeader = (props) => (
                                 </div>:""
                             }                         
                          </h1>
+                         {props.state.showBackButton?
+                         <button className="btn btn-success mt-2 back-button px-md-3" 
+                                 onClick={()=>props.history.goBack()}><i className="fas fa-backward mr-2"></i> Back</button>:""}
                     </div>
-                </div>
-                <div className="row">
-                        
+                    </div>
                 </div>
             </div>
 );
