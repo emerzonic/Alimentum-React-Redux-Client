8d14dfb91f47f40bcb9969291fd3015f88dd9e18
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 21:47:44 -0600

    user logic

--- src/components/pages/user/User.js
+++ src/components/pages/user/User.js
@@ -1,99 +1,129 @@
-// import React from 'react';
-// import {
-//     Component
-// } from 'react';
-// import API from "../../../API";
-// import Login from "../Home/Login";
-// import SignUp from "./Sign-up";
-// import Signout from "./Sign-out";
-// import Nav from "../Home/Nav";
-// import Footer from "../Home/Footer";
+import React from 'react';
+import {
+    Component
+} from 'react';
+
 // import './home.css';
-
-// class User extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             user: {
-//                 _id: '',
-//                 username: ''
-//             }
-//         };
-
-//         //Fires when the search form is submitted
-//         this.handleUserSignup = (newUser) => {
-//             API.sendNewUserData(newUser).then(res => {
-//                 if (res.data.username) {
-//                     localStorage.setItem('erapp_user', res.data.username)
-//                     localStorage.setItem('erapp_id', res.data._id)
-//                     this.setState({
-//                         user: {
-//                             id: res.data._id,
-//                             username: localStorage.getItem('erapp_user')
-//                         }
-//                     })
-//                     this.props.history.push('/search');
-//                 } else {
-//                     this.props.history.push('/signup');
-//                 }
-//             }).catch(err => console.log(err));
-//         }
+import UserForm from "./UserForm"
+import axios from 'axios';
 
 
-//         //Fires when the search form is submitted
-//         this.handleUserSignin = (preciousUser) => {
-//             API.sendPreviousUserData(preciousUser).then(res => {
-//                 if (res.data.username) {
-//                     localStorage.setItem('erapp_user', res.data.username)
-//                     localStorage.setItem('erapp_id', res.data._id)
-//                     this.setState({
-//                         user: {
-//                             id: res.data._id,
-//                             username: localStorage.getItem('erapp_user')
-//                         }
-//                     })
-//                     this.props.history.push('/search');
-//                 } else {
-//                     this.props.history.push('/login');
-//                 }
-//             }).catch(err => console.log(err));
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newUser:{
+                password: '',
+                username: ''
+            },
+            user:{
+                password: '',
+                username: ''
+            },
+        };
 
-//         }
+        this.handleSignUpInputChange = (e) => {
+            let target = e.target;
+            let value = target.value;
+            let name  = target.name;
+            this.setState({
+                newUser:{
+                    [name]:value}
+            })
+        }
 
-//         //This method handle user signout
-//         this.handleUserSignout= (action) => {
-//             if(action === "positive"){
-//             localStorage.removeItem('erapp_id');
-//             localStorage.removeItem('erapp_user');
-//             localStorage.removeItem('erapp_articles');
-//             this.props.history.push('/');
-//             }else{
-//             this.props.history.goBack();
-//             }
-//         }
+        this.handleLoginInputChange = (e) => {
+            let target = e.target;
+            let value = target.value;
+            let name  = target.name;
+            this.setState({
+                user:{
+                    [name]:value}
+            })
+        }
 
-//     }
+        this.handleSubmit = e =>{
+            e.preventDefault();
+            let user = {
+                // fullName: e.target.fullName.value,
+                username: e.target.username.value,
+                password: e.target.password.value
+            };
+            console.log(user);
+            if(!user.password.length ||!user.password.length){ 
+              return;
+          }else{
+            let requestType = e.target.getAttribute("request-type");
+              if(requestType==="signUp"){
+                this.createNewUser(user);
+              }else{
+                this.loginUser(user);
+              }
+            }
+          }
 
-//     render() {
-//         return (
-//             <div>
-//             <Nav user={this.state.user.username}/>
-//                 <div className="ui container user-container">
-//                     {this.props.match.path === '/signin'?
-//                     <div className="ui segment">
-//                         <Login handleUserSignin={this.handleUserSignin}/>
-//                     </div>:
-//                         this.props.match.path === '/signup'?
-//                         <div className="ui segment">
-//                         <SignUp handleUserSignup={this.handleUserSignup}/>
-//                         </div>:""}
-//                         {this.props.match.path === '/signout'?
-//                         <Signout handleUserSignout={this.handleUserSignout}/>:''} 
-//                 </div>
-//             <Footer/>
-//         </div>
-//         );
-//     }
-// }
+        this.createNewUser = (newUser) => {
+            axios.post("http://localhost:5000/user/signup", newUser).then(res => {
+                console.log(res);
+                // if (res.data.username) {
+                //     localStorage.setItem('erapp_user', res.data.username)
+                //     localStorage.setItem('erapp_id', res.data._id)
+                //     this.setState({
+                //         user: {
+                //             id: res.data._id,
+                //             username: localStorage.getItem('erapp_user')
+                //         }
+                //     })
+                //     this.props.history.push('/search');
+                // } else {
+                //     this.props.history.push('/signup');
+                // }
+            }).catch(err => console.log(err));
+        }
 
-// export default User;
+        this.loginUser = (User) => {
+            axios.post("http://localhost:5000/login", User).then(res => {
+                if (res.data.username) {
+                    //             localStorage.setItem('erapp_user', res.data.username)
+                    //             localStorage.setItem('erapp_id', res.data._id)
+                    //             this.setState({
+                    //                 user: {
+                    //                     id: res.data._id,
+                    //                     username: localStorage.getItem('erapp_user')
+                    //                 }
+                    //             })
+                    //             this.props.history.push('/search');
+                    //         } else {
+                    //             this.props.history.push('/login');
+                            }
+            }).catch(err => console.log(err));
+
+        }
+        //This method handle user signout
+    //     this.handleUserSignout= (action) => {
+    //         if(action === "positive"){
+    //         localStorage.removeItem('erapp_id');
+    //         localStorage.removeItem('erapp_user');
+    //         localStorage.removeItem('erapp_articles');
+    //         this.props.history.push('/');
+    //         }else{
+    //         this.props.history.goBack();
+    //         }
+    //     }
+
+    }
+
+    render() {
+console.log(this.state);
+        return (
+            <div>
+            <UserForm onSubmit={this.handleSubmit} 
+                      signUpChange={this.handleSignUpInputChange}
+                      loginChange={this.handleLoginInputChange}
+                      state={this.state} />
+        </div>
+        );
+    }
+}
+
+export default User;
