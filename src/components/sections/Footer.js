8b8497b8d4e2f5fe7ab12a3ae222a9d1b45d1965
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 21:48:14 -0600

    refactor nav

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../pages/home/home.css';
 
 const Footer = () => (
-    <nav className="nav navbar-light text-center">
+    <nav className="nav navbar-light bg-dark text-center">
         <span className="item text-light p-5">Built with Spring Boot REST Service backend & React JS client. Copyright &copy;2018 Emerson Doyah Projects</span>
     </nav> 
 );
