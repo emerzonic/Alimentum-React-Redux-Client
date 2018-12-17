9a75f22b5a2c7d328c571743981d4761c7c346fe
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-16 21:58:04 -0600

    add page pusher

---
+++ src/components/sections/Pusher.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../pages/home/home.css';
+
+const Pusher = () => (
+   <div className="container bg-white shadow-sm my-2 pusher">
+   </div>
+);
+
+export default Pusher;
