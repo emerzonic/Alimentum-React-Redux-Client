096d0192276828c9e01208b84285a8e0fce1331d
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-20 19:48:39 -0600

    add spring and react icons

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -3,7 +3,8 @@ import '../pages/home/home.css';
 
 const Footer = () => (
     <nav className="nav navbar-light bg-dark text-center">
-        <span className="item text-light p-5">Built with Spring Boot REST Service backend & React JS client. Copyright &copy;2018 Emerson Doyah Projects</span>
+        <span className="item text-light p-md-5"><i className="fas fa-leaf text-success"></i><i className="fab fa-react ml-2"></i> Built with Spring Boot REST Service backend & React JS client. </span>
+        <span className="item text-light p-md-5"> Copyright &copy;2018 Emerson Doyah Projects</span>
     </nav> 
 );
 
