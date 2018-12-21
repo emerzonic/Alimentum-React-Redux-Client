cc61e7166a85ab3329dfc83e8a48430d446cbc77
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-20 19:44:07 -0600

    css link

--- src/components/pages/category/Category.js
+++ src/components/pages/category/Category.js
@@ -5,7 +5,7 @@ import assets from '../../../assets';
 import util from '../../util';
 import '../home/home.css';
 import PageHeader from '../../sections/Page Header';
-
+import "./category.css";
 
 class  Category extends Component {
     constructor(props) {
@@ -35,7 +35,10 @@ class  Category extends Component {
                         <div className="row">
                         {this.state.categories.map((item, i) => {
                             return <div className="card col-md-4 my-2 border-0 shadow-sm category-div" key={i}>
-                                      <img className="card-img-top mt-3 rounded" src={item.image} alt={this.props.name}/>
+                                      <img className="card-img-top mt-3 rounded" 
+                                           src={item.image} alt={this.props.name}
+                                           onClick={()=>this.props.history.push(`/categories/${item.category}`)}
+                                           />
                                       <div className="card-body">
                                         <h5 className="card-title category-title">{item.category}</h5>
                                         <button className="btn btn-outline-success col-sm-12" data-type="category" 
