e334c129f481dc6d06e6d81dbf8840afe81162cc
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-20 19:49:13 -0600

    nav refactor

--- src/components/sections/Nav.js
+++ src/components/sections/Nav.js
@@ -9,14 +9,14 @@ const Nav = () => (
                         data-target="#navbarNavDropdown" 
                         aria-controls="navbarNavDropdown" 
                         aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon text-white"></span>
+                    <span className="text-white"><i className="fas fa-bars"></i></span>
                     <span className="text-white ml-2">Menu</span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <a href="/categories" className="nav-link">Categories</a>
                     <a href="/user/favorites" className="nav-link">Favorites</a>
-                    <a href="/user/loginOrSignup" className="nav-link justify-self-end">Login</a>
-                    <a href="/user/loginOrSignup" className="nav-link justify-self-end">Sign Up</a>
+                    <a href="/user/login" className="nav-link justify-self-end">Login</a>
+                    <a href="/user/signup" className="nav-link justify-self-end">Sign Up</a>
                     <a href="/" className="nav-link justify-self-end">Logout</a>
                 </div>
             </nav>
