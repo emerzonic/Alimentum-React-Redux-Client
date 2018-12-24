cdea0607fc01ad409cf1ffd3af4ad0bffbde0a4e
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-24 15:07:48 -0600

    Added redux

--- src/components/pages/recipeDetail/RecipeDetail.js
+++ src/components/pages/recipeDetail/RecipeDetail.js
@@ -4,8 +4,14 @@ import Modal from '../../sections/Modal';
 import Header from '../../sections/Header';
 import PageHeader from '../../sections/Page Header';
 import Img from '../../../assets';
-import axios from 'axios';
-import util from '../../util';
+// import axios from 'axios';
+// import util from '../../util';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { saveRecipe } from "../../../actions/projectActions";
+import { getRecipeById } from "../../../actions/projectActions";
+import { updatePageTitle } from "../../../actions/projectActions";
+
 import '../home/home.css';
 import "./detail.css";
 
@@ -15,74 +21,87 @@ let img = {
     backgroundSize:"cover",
     height:"6rem"
   };
-class recipe extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            recipe:[],
-            pageTitle:"",
-            saveFeedBack:"",
-        }
+class Recipe extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.props = { 
+    //         recipe:{}||[],
+    //         pageTitle:"",
+    //         saveFeedBack:"",
+    //         errors:{}
+    //     }
 
-    this.getRecipeById = () =>{ 
-        let recipeId = this.props.match.params.recipeId;
-        axios.get(`http://localhost:5000/api/searchByRecipeId/${recipeId}`).then(res => {
-        let refinedRecipe = util.getRecipeObj(res.data[0]);
-            this.setState({
-                recipe:refinedRecipe,
-                pageTitle:`Recipe Detail for "${refinedRecipe.strMeal}".`
-            })
-        }).catch(err => console.log(err));
-    };
+    // this.getRecipeById = () =>{ 
+        // let recipeId = "52807";
+        
 
-    this.saveRecipe = () =>{  
-        let recipe = this.state.recipe;
-        let username = "emerson";
-        axios.post(`http://localhost:5000/currentUser/saveRecipe/${username}`, recipe).then(res => {
-            this.setState({
-                saveFeedBack:res.data.text,
-            })            
-        }).catch(err => console.log(err));
+        // axios.get(`http://localhost:5000/api/recipes/searchByRecipeId/${recipeId}`).then(res => {
+        // let refinedRecipe = util.getRecipeObj(res.data[0]);
+        //     this.setState({
+        //         recipe:refinedRecipe,
+        //         pageTitle:`Recipe Detail for "${refinedRecipe.strMeal}".`
+        //     })
+        // }).catch(err => console.log(err));
+    // };
+
+
+    saveRecipe = () =>{  
+        let recipe = this.props.recipe;
+        let userId = 2;
+        this.props.saveRecipe(recipe, userId)
+
+        // axios.post(`http://localhost:5000/api/currentUser/saveRecipe/${userId}`, recipe).then(res => {
+        //     this.setState({
+        //         saveFeedBack:res.data.text,
+        //     })            
+        // }).catch(err => console.log(err));
     };
-}
-    
+// }
+    // componentWillReceiveProps(nextProps){
+    //     // if (nextProps.errors) {
+    //         this.setState({
+    //             errors:nextProps.errors,
+    //             saveFeedBack:nextProps.saveFeedback,
+    //             recipe:nextProps.recipe
+    //         })
+    //     // }
+    // }
     componentDidMount(){
-      this.getRecipeById();
+        let recipeId = this.props.match.params.recipeId;
+        let recipe = this.props.match.params.recipe;
+        this.props.getRecipeById(recipeId);
+        this.props.updatePageTitle(recipe);
     }
   render() {
+      console.log(this.props)
       return <div>
-                <Header {...this.props} state={this.state}/>
-                <PageHeader {...this.props} state={this.state}/>
+                <Header/>
+                <PageHeader {...this.props}/>
                 <div className="container px-0 my-2 items-container" >
                  <div className="card mb-3 recipe-detail-div border-0 shadow-sm p-md-4">
                 <div className="row">
                       <div className="col-md-8 pl-0">
-                          <img className="card-img-top img-fluid rounded ml-md-4" src={this.state.recipe.strMealThumb} alt={this.props.name}/>
+                          <img className="card-img-top img-fluid rounded ml-md-4" src={this.props.recipe.strMealThumb} alt={this.props.name}/>
                       </div>
-                      <div className="col-md-4">
-                      <div className="card text-white border-0">
-                      <img className="card-img youtube" data-toggle="modal" data-target="#exampleModal" src={Img.youtube.img} alt="Card"/>
-                      <div className="card-img-overlay">
-                      </div>
-                    </div>
-                            <button className="mx-1 w-100 m-2 youtube" style={img} data-toggle="modal" data-target="#exampleModal">Watch Video</button>
-                            <button className="btn btn-primary mx-1 w-100 m-2" onClick={this.saveRecipe}>Save Recipe</button>
-                            <a href={this.state.recipe.strSource} className="btn btn-primary mx-1 w-100 m-2" target="_blank" rel="noopener noreferrer">Read More</a>
+                    <div className="col-md-4">
+                            <button className="mx-1 w-100 m-2 youtube border-0 btn-lg" style={img} data-toggle="modal" data-target="#exampleModal">Watch Video</button>
+                            <button className="btn btn-lg btn-primary mx-1 w-100 m-2" onClick={this.saveRecipe}>Save Recipe</button>
+                            <a href={this.props.recipe.strSource} className="btn btn-lg btn-primary mx-1 w-100 m-2" target="_blank" rel="noopener noreferrer">Read More</a>
                       </div>
                   </div>
                   <div className="card-body">
                       <div className="row">
                           <div className="col-md-12 pl-0">
-                              <h5 className="card-title display-4"><i className="far fa-flag"></i> {this.state.recipe.strArea}</h5>
+                              <h5 className="card-title display-4"><i className="far fa-flag"></i> {this.props.recipe.strArea}</h5>
                           </div>
                       </div>
                       <div className="row">
                           <div className="col-md-8 pl-0">
                           <h5 className="card-title">Instructions</h5>
                           <ul className="card-text bg-light p-4">
-                                {this.state.recipe.strInstructions?
-                                    this.state.recipe.strInstructions.map((instr, i)=>{
-                                    return <li className="instruction" key={i}><i className="fas fa-asterisk"></i> {instr}</li>
+                                {this.props.recipe.strInstructions?
+                                    this.props.recipe.strInstructions.map((instr, i)=>{
+                                    return(<div className="p-2 bg-light" key={i}><i className="fas fa-check-square mr-2 d-inline"></i><li className="instruction d-inline">{instr}</li></div> )
                                 }):""}
                             </ul>
                           </div>
@@ -90,9 +109,9 @@ class recipe extends Component {
                         <h5 className="card-title"><i className="fas fa-carrot"></i> Ingredients</h5>
                           <div className="card w-100 mr-2">
                               <ul className="list-group list-group-flush border-0">
-                                {this.state.recipe.strIngredients?
-                                    this.state.recipe.strIngredients.map((ing, i)=>{
-                                  return <li className="list-group-item ingredient py-1" key={i}>{this.state.recipe.strMeasurements[i]} {ing}</li>
+                                {this.props.recipe.strIngredients?
+                                    this.props.recipe.strIngredients.map((ing, i)=>{
+                                  return <li className="list-group-item ingredient py-1" key={i}>{this.props.recipe.strMeasurements[i]} {ing}</li>
                                 }):""}
                               </ul>
                           </div>
@@ -101,9 +120,26 @@ class recipe extends Component {
                   </div>
                 </div>
             </div>
-        <Modal recipe={this.state.recipe}/>                  
+        <Modal recipe={this.props.recipe}/>                  
     </div>
       }
 }
+
+Recipe.propTypes = {
+    saveRecipe:PropTypes.func.isRequired,
+    getRecipeById:PropTypes.func.isRequired,
+    updatePageTitle:PropTypes.func.isRequired,
+    errors:PropTypes.object,
+    saveFeedBack:PropTypes.string,
+    recipe:PropTypes.object,
+    pageTitle:PropTypes.string.isRequired
+}
+
+const mapStateToProps = state =>({
+    errors:state.error,
+    saveFeedBack:state.saveFeedBack,
+    recipe:state.recipe,
+    pageTitle:state.pageTitle
+})
  
-export default recipe;
+export default connect(mapStateToProps,{saveRecipe,getRecipeById, updatePageTitle})(Recipe);
