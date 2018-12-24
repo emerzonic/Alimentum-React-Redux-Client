46c9b162e5f8dae31efa6cf6bc8fae817bc95098
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-24 15:11:08 -0600

    Added redux

--- src/components/sections/Page Header.js
+++ src/components/sections/Page Header.js
@@ -8,15 +8,15 @@ const PageHeader = (props) => (
                 <div className="row">
                     <div className="col-12 mx-auto p-0">
                     <div className="rounded shadow-sm p-3 m-0 page-title-div">
-                         <h1 className="display-4 page-title mb-0">{props.state.pageTitle}
-                            {props.state.saveFeedBack? 
+                         <h1 className="display-4 page-title mb-0">{props.pageTitle}
+                            {props.saveFeedBack? 
                                 <div className="alert alert-success save-feedback d-md-inline ml-md-5" role="alert">
                                 <i className="fas fa-check mr-2"></i>
-                                    {props.state.saveFeedBack}
+                                    {props.saveFeedBack}
                                 </div>:""
                             }                         
                          </h1>
-                         {props.state.showBackButton?
+                         {props.showBackButton?
                          <button className="btn btn-success mt-2 back-button px-md-3" 
                                  onClick={()=>props.history.goBack()}><i className="fas fa-backward mr-2"></i> Back</button>:""}
                     </div>
