f5f621cbd7f49943b5286de5309fbdd9bc0d18ec
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-24 15:07:36 -0600

    Added redux

--- src/components/pages/category/Category.js
+++ src/components/pages/category/Category.js
@@ -1,39 +1,29 @@
 import React from 'react';
 import { Component } from 'react';
-import Header from "../../sections/Header";
-import assets from '../../../assets';
-import util from '../../util';
-import '../home/home.css';
+import { connect } from 'react-redux';
+import { getCategories, updatePageTitle } from "../../../actions/projectActions";
 import PageHeader from '../../sections/Page Header';
+import Header from "../../sections/Header";
+import Loading from '../../sections/Loading';
+import PropTypes from "prop-types";
+import '../home/home.css';
 import "./category.css";
 
 class  Category extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            categories:[],
-            recipes:[],
-            recipeDetail:{},
-            pageTitle:"",
-            saveFeedBack:"",
-            unsaveFeedBack:"",
-            name:"",
-            isLoading:true
-        }
 
-     this.state.categories = util.getDataArray(assets.category); 
-     this.state.pageTitle = "Categories";
-     this.state.isLoading = false;
-
-}
+componentDidMount(){
+    this.props.getCategories();
+    this.props.updatePageTitle("Recipe Categories");
+  }
     render() { 
         return (
             <div>
-                <Header {...this.props} state={this.state}/>
-                <PageHeader {...this.props} state={this.state}/>
+                <Header {...this.props}/>
+                <PageHeader {...this.props}/>
                     <div className="container items-container">
                         <div className="row">
-                        {this.state.categories.map((item, i) => {
+                        {!this.props.categories.length?<Loading/>:
+                            this.props.categories.map((item, i) => {
                             return <div className="card col-md-4 my-2 border-0 shadow-sm category-div" key={i}>
                                       <img className="card-img-top mt-3 rounded" 
                                            src={item.image} alt={this.props.name}
@@ -53,4 +43,17 @@ class  Category extends Component {
     }
 }
  
-export default Category;
+Category.propTypes = {
+    updatePageTitle:PropTypes.func.isRequired,
+    getCategories:PropTypes.func.isRequired,
+    pageTitle:PropTypes.string,
+    errors:PropTypes.object,
+    categories:PropTypes.array.isRequired,
+    }
+  
+  const mapStateToProps = state =>({
+      errors:state.error,
+      categories:state.categories,
+      pageTitle:state.pageTitle
+  })
+export default connect(mapStateToProps,{getCategories, updatePageTitle})(Category);
