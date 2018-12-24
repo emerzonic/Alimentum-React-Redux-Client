11ea77f0f68423b3d581579122b1eb072a0b4132
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-24 15:08:06 -0600

    Added redux

--- src/components/pages/search/Results.js
+++ src/components/pages/search/Results.js
@@ -5,56 +5,37 @@ import '../home/home.css';
 import PageHeader from '../../sections/Page Header';
 import Pusher from '../../sections/Pusher';
 import Loading from '../../sections/Loading';
-
-
-
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { updatePageTitle } from "../../../actions/projectActions";
 
 class Results extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-        recipes:[],
-        pageTitle:'',
-        showBackButton:false,
-        isLoading:true
-    }
-
-this.getRecipesByCategory = () =>{ 
-    let recipes = this.props.location.state.recipes; 
-    let pageTitle = this.props.location.state.pageTitle; 
-        this.setState({
-            recipes: recipes,
-            showBackButton:true,
-            isLoading:false,
-            pageTitle:recipes?`Showing results for "${pageTitle}".`:
-            `There are no recipes found for "${pageTitle}".`
-
-        })
-    }
-}
 
 componentDidMount(){
-  this.getRecipesByCategory()
-}
+    let searchTerm = this.props.match.params.searchTerm;
+    let pageTitle = this.props.recipes.length?
+    `Showing results for "${searchTerm}".`:
+    `There are no recipes found for "${searchTerm}".`;
+    this.props.updatePageTitle(pageTitle);
+  }
 
   render() {
       return <div>
-                <PageHeader {...this.props} state={this.state}/>
+                <PageHeader {...this.props}/>
                 <div className="container">   
                     <div className="row">
-                    {this.state.isLoading? <Loading/>:""}
-                        {this.state.recipes.length? this.state.recipes.map(item => 
+                    {!this.props.recipes.length? <Pusher/>:
+                        this.props.recipes.map(item => 
                           <div className="card col-md-4 my-2 border-0 shadow-sm recipe-div" key={item.idMeal}>
                             <img className="card-img-top rounded-circle mt-3"  
                                 data-type="name"
                                 src={item.strMealThumb} alt={this.props.name} 
-                                onClick={()=>this.props.history.push(`/categories/${item.strCategory}/${item.idMeal}`)}/>
+                                onClick={()=>this.props.history.push(`/categories/${item.strCategory}/${item.strMeal}/${item.idMeal}`)}/>
                             <div className="card-body recipe-title">
                               <h5 className="card-title text-center">{item.strMeal}</h5>
                             </div>
                           </div>
-                        ):
-                    <Pusher/>
+                        )
                     }
                     </div>
                   </div>
@@ -62,4 +43,18 @@ componentDidMount(){
               }
     }
  
-export default Results;
+Results.propTypes = {
+        updatePageTitle:PropTypes.func.isRequired,
+        pageTitle:PropTypes.string,
+        errors:PropTypes.object,
+        recipes:PropTypes.array.isRequired,
+        // showBackButton:PropTypes.boolean,
+        }
+      
+const mapStateToProps = state =>({
+          errors:state.error,
+          recipes:state.recipes,
+          pageTitle:state.pageTitle
+
+      })
+export default connect(mapStateToProps,{updatePageTitle}) (Results);
