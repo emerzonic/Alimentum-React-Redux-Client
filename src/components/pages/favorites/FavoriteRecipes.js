868a990f5f24092d8e2e68a0aa7e4934f9113c79
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 09:06:40 -0600

    refactored favorite recipes

--- src/components/pages/favorites/FavoriteRecipes.js
+++ src/components/pages/favorites/FavoriteRecipes.js
@@ -7,8 +7,8 @@ import "./favorite.css";
 
 class FavoriteRecipes extends Component {
   render() {
-    let recipes = this.props.state.recipes.map(item => {
-      return <div className="media my-3 p-md-3" key={item.idMeal} onClick={this.props.onClick}>
+    let recipes = this.props.favoriteRecipes.map(item => {
+      return <div className="media my-3 p-md-3 fav-recipe" key={item.idMeal} onClick={this.props.onClick}>
                 <img className="mr-3 rounded-circle img-fluid
                          col-sm-12" src={item.strMealThumb}  alt={this.props.name}/>
                 <div className="media-body col-sm-12">
@@ -18,7 +18,7 @@ class FavoriteRecipes extends Component {
                     <button type="button" className="btn btn-outline-danger  m-1" data-id={item.id}
                             onClick={this.props.deleteRecipe}> <i className="fas fa-trash-alt"></i> Delete</button>
                     <button type="button" className="btn btn-outline-success m-1" data-id={item.id} 
-                            onClick={()=>this.props.history.push(`/categories/${item.strCategory}/${item.idMeal}`)}><i className="fas fa-clipboard-list"></i> See Detail</button>
+                            onClick={()=>this.props.history.push(`/categories/${item.strCategory}/${item.strMeal}/${item.idMeal}`)}><i className="fas fa-clipboard-list"></i> See Detail</button>
                 </div>
             </div>
         });
