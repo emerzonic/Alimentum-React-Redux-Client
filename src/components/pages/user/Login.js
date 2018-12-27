f1d3b88cdb2c8bc40392c2bb459a2b06e208b4f5
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 09:08:07 -0600

    moved state to redux

--- src/components/pages/user/Login.js
+++ src/components/pages/user/Login.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
+import { loginUser } from "../../../actions/projectActions";
+import PropTypes from "prop-types";
 
 import './user.css';
 class  Login extends Component {
@@ -8,9 +11,8 @@ class  Login extends Component {
         this.state = {
                 username: '',
                 password: '',
-                usernameError:false,
-                passwordError:false
-        }
+                errors:{}
+            }
         
         this.handleOnChange = (e) => {
             let target = e.target;
@@ -22,52 +24,78 @@ class  Login extends Component {
         }
 
         this.handleSubmit = e =>{
-            this.setState({ usernameError:false, passwordError:false })
             e.preventDefault();
             let user = {
-                username: e.target.username.value,
-                password: e.target.password.value
+                "username": e.target.username.value,
+                "password": e.target.password.value,
             };
-            // let usernameError = user.username.length ? false : true;
-            // let passwordError = user.password.length ? false : true;
-            // if(usernameError || passwordError){ 
-            //     this.setState({
-            //             usernameError:usernameError,
-            //             passwordError:passwordError
-            //     })
-            //   return;
-            // }else{
-            let requestType = e.target.getAttribute("request-type");
+            const history = this.props.history;
             e.target.reset();
-            this.props.onSubmit(user, requestType);
-            // }
+            this.props.loginUser(user, history)
         }
+
+        
 }
+
+componentWillReceiveProps(nextProps){
+    if(nextProps.errors){
+      this.setState({errors: nextProps.errors });
+    }
+    if(nextProps.currentUser.validToken){
+      this.props.history.push("/categories")
+    }
+  }
     render() { 
-        return (<div className={this.props.form==="login"?"tab-pane fade show active":"tab-pane fade"} id="login" role="tabpanel" aria-labelledby="login-tab">
-                    <form className="p-md-5 mb-5 shadow-sm border rounded-bottom" onSubmit={this.handleSubmit} request-type="login">
-                        <h1 className="card-title">Login</h1>
+        const {errors} = this.state;
+        const invalidClass = "form-control is-invalid";
+        const validClass = "form-control";
+        return (
+            <div className="container">
+            <div className="row">
+                <div className="col-md-6 col-sm-12 mx-auto user-form">                     
+                <form className="user-forms shadow-sm border rounded" onSubmit={this.handleSubmit} request-type="login">
+                <h1 className="card-title">Login</h1>
+                <small className="error-text form-text text-muted">
+                        {errors?errors.credentials:""}
+                </small>
                         <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" className="form-control" id="login-username" aria-describedby="username" placeholder="Enter Username"  
+                            <label className="form-label" htmlFor="username">Username</label>
+                            <input type="text" className={errors.username?invalidClass:validClass} id="login-username" aria-describedby="username" placeholder="Enter Username"  
                                    name="username" value={this.state.username} onChange={this.handleOnChange}/>
-                            <small id="usernameError" className="form-text text-muted text-danger">
-                                   {this.state.usernameError?"Username cannot be empty!":" "}
+                            <small className="error-text form-text text-muted">
+                                   {errors?errors.username:""}
                             </small>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="login-password"  placeholder="Enter Password" 
+                            <label className="form-label" htmlFor="exampleInputPassword1">Password</label>
+                            <input type="password" className={errors.password?invalidClass:validClass} id="login-password"  placeholder="Enter Password" 
                                     name="password" value={this.state.password}  onChange={this.handleOnChange}/>
-                            <small id="usernameError" className="form-text text-muted text-danger">
-                                   {this.state.passwordError?"Password cannot be empty!":" "}
+                            <small className="error-text form-text text-muted">
+                                    {errors?errors.password:""}
                             </small>
                         </div>
-                            <button type="submit" className="btn btn-primary">Login</button>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <p className="d-inline mx-2"> Don't have an account?</p>
+                            <a className="d-inline user-links"  href="/user-form/signup"> Sign UP</a>
                     </form>
+                </div>
+                </div>
                 </div>
             );
         }
 }
 
-export default Login;
+Login.propTypes = {
+    loginUser:PropTypes.func.isRequired,
+    errors:PropTypes.object.isRequired,
+    currentUser:PropTypes.object.isRequired
+}
+
+const mapStateToProps = state =>({
+    errors:state.errors,
+    currentUser:state.currentUser
+
+})
+export default connect(mapStateToProps, {
+    loginUser
+})(Login);
