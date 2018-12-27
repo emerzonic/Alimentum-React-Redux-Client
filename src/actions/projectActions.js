4f09df5424365f1bfba9709572c2af65e2558c7e
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 08:59:37 -0600

    added dependency

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,23 +1,38 @@
-import axios from "axios";
+import {setJwtToken, updateJwtToken} from "../securityUtlis/jwtToken";
 import util from "../components/util"
 import assets from "../assets"
-import {GET_ERRORS, GET_CURRENT_USER_DATA, 
-    GET_RECIPES, GET_SAVE_FEEDBACK, 
-    GET_RECIPE_BY_ID, GET_PAGE_TITLE,GET_CATEGORIES} from "./types";
-// import promiseMiddleware from 'redux-promise';
+import axios from "axios";
+import {
+    GET_ERRORS, GET_RECIPES, GET_SAVE_FEEDBACK, 
+    GET_RECIPE_BY_ID, GET_PAGE_TITLE,
+    GET_CATEGORIES,SET_MENUS,SET_CURRENT_USER,
+    GET_USER_RECIPES, DELETE_FEEDBACK, SHOW_BACK_BUTTON
+} from "./types";
+
+//==============================================================
+//SETUP HOME PAGE MENU
+//==============================================================
+export const getMenus = () => async dispatch => {
+    let homePageMenus = util.getDataArray(assets.homeCardsObj)
+        dispatch({
+            type:SET_MENUS, 
+            payload:homePageMenus
+        })
+    }
 
 
+//==============================================================
+//SEARCH RECIPE ACTION
+//==============================================================
 export const getRecipeBySearchTerm = (searchTerm, history) => async dispatch => {
     try{
-        const res = await axios.get(`http://localhost:5000/api/recipes/searchByName/${searchTerm}`)
-        console.log(res.data)
+        const res = await axios.get(`/api/recipes/searchByName/${searchTerm}`)
         dispatch({
             type:GET_RECIPES, 
-            payload:res.data
+            payload:res.data || []
         })
         history.push(`/search/${searchTerm}/results`)
     }catch(err){
-        console.log(err)
         dispatch({
             type:GET_ERRORS,
             payload:err.response.data
@@ -25,40 +40,36 @@ export const getRecipeBySearchTerm = (searchTerm, history) => async dispatch => 
     }
 }
 
-
+//==============================================================
+//GET RECIPES BY CATEGORIES ACTION
+//==============================================================
 export const getCategories = () => async dispatch => {
     let categories = util.getDataArray(assets.category)
-    try{
         dispatch({
             type:GET_CATEGORIES, 
             payload:categories
         })
-    }catch(err){
-        dispatch({
-            type:GET_ERRORS,
-            payload:err.response.data
-        })
-}
 }
 
 
 export const updatePageTitle = (pageTitle) => async dispatch => {
-    try{
         dispatch({
             type:GET_PAGE_TITLE, 
-            payload:`${pageTitle}`
-        })
-    }catch(err){
-        dispatch({
-            type:GET_ERRORS,
-            payload:err.response.data
+            payload:pageTitle
         })
 }
+
+export const updateBackButton = (status) => async dispatch => {
+    dispatch({
+        type:SHOW_BACK_BUTTON, 
+        payload:status
+    })
 }
+
 
 export const getRecipesByCategory = (category) => async dispatch => {
     try{
-        const res = await axios.get(`http://localhost:5000/api/recipes/searchByCategory/${category}`)
+        const res = await axios.get(`/api/recipes/searchByCategory/${category}`)
         dispatch({
             type:GET_RECIPES,
             payload:res.data
@@ -76,7 +87,7 @@ export const getRecipesByCategory = (category) => async dispatch => {
 //==============================================================
 export const getRecipeById = (recipeId) => async dispatch => {
     try{
-        const res = await axios.get(`http://localhost:5000/api/recipes/searchByRecipeId/${recipeId}`)
+        const res = await axios.get(`/api/recipes/searchByRecipeId/${recipeId}`)
         let refinedRecipe = util.getRecipeObj(res.data[0]);
         dispatch({
             type:GET_RECIPE_BY_ID, 
@@ -92,10 +103,10 @@ export const getRecipeById = (recipeId) => async dispatch => {
 
 export const saveRecipe = (recipe, userId) => async dispatch => {
     try{
-        const res = await axios.post(`http://localhost:5000/api/currentUser/saveRecipe/${userId}`, recipe)
+        const res = await axios.post(`/api/currentUser/saveRecipe/${userId}`, recipe)
         dispatch({
             type:GET_SAVE_FEEDBACK,
-            payload:res.data.text
+            payload:res.data
         })
     }catch(err){
         dispatch({
@@ -107,11 +118,11 @@ export const saveRecipe = (recipe, userId) => async dispatch => {
 
 
 //==============================================================
-//USER SIGNUP & LOGIN ACTIONS
+//SECURITY ACTIONS
 //==============================================================
 export const createUser = (newUser, history) => async dispatch => {
     try{
-        const res = await  axios.post("http://localhost:5000/api/users/signup", newUser)
+        const res = await  axios.post("/api/users/signup", newUser)
        history.push('/user-form/login');
     }catch(err){
         dispatch({
@@ -121,14 +132,27 @@ export const createUser = (newUser, history) => async dispatch => {
     }
 }
 
-export const loginUser = (user, history) => async dispatch => {
+export const loginUser = (user) => async dispatch => {
     try{
-        const res = await  axios.post("http://localhost:5000/api/users/login", user)
+        const res = await  axios.post("/api/users/login", user)
+        const {token} = res.data;
+        localStorage.setItem("JwtToken", token);
+        setJwtToken(token)
+        updateJwtToken(token, dispatch,SET_CURRENT_USER )
+    }catch(err){
         dispatch({
-            type:GET_CURRENT_USER_DATA,
-            payload:res.data
+            type:GET_ERRORS,
+            payload:err.response.data
         })
-        history.push('/');
+    }
+}
+
+export const logoutUser = (history) => async dispatch => {
+    try{
+        localStorage.removeItem("JwtToken");
+        setJwtToken(false)
+        updateJwtToken(false, dispatch,SET_CURRENT_USER )
+        history.push("/user-form/login")
     }catch(err){
         dispatch({
             type:GET_ERRORS,
@@ -140,3 +164,41 @@ export const loginUser = (user, history) => async dispatch => {
 //==============================================================
 //USER FAVORITE RECIPES ACTIONS
 //==============================================================
+export const getUserRecipes = (userId) => async dispatch => {
+    try{
+       const res = await axios.get(`/api/currentUser/getUserRecipes/${userId}`)
+        dispatch({
+            type:GET_USER_RECIPES, 
+            payload:res.data
+        })    
+    }catch(err){
+        dispatch({
+            type:GET_ERRORS,
+            payload:err.response.data
+        })
+    }
+}
+
+export const deleteRecipe = (recipeId, userId) => async dispatch => {
+    dispatch({
+        type:DELETE_FEEDBACK,
+        payload:{}
+    })
+    try{
+        const deleteResponse = await axios.delete(`/api/currentUser/deleteRecipe/${userId}/${recipeId}`)
+        const getRecipesResonse = await axios.get(`/api/currentUser/getUserRecipes/${userId}`)
+        dispatch({
+            type:GET_USER_RECIPES, 
+            payload:getRecipesResonse.data
+        })
+        dispatch({
+            type:DELETE_FEEDBACK,
+            payload:deleteResponse.data
+        })
+    }catch(err){
+        dispatch({
+            type:GET_ERRORS,
+            payload:err.response.data
+        })
+    }
+}
