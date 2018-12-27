5c2b48f801b732f8ce8987125e09750c1ad66225
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 09:10:24 -0600

    moved state to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,16 +3,24 @@ import errorReducer from "./errorReducer";
 import saveFeedbackReducer from "./saveFeedbackReducer";
 import recipeReducer from "./recipeReducer";
 import recipesReducer from "./recipesReducer";
-import createUserReducer from "./createUserReducer";
 import pageTitleReducer from "./pageTitleReducer";
 import categoriesReducer from "./categoriesReducer";
+import menuReducer from "./menuReducer";
+import currentUserReducer from "./currentUserReducer";
+import favoriteRecipesReducer from "./favoriteRecipesReducer";
+import deleteRecipeReducer from "./deleteRecipeReducer";
+import backButtonReducer from "./backButtonReducer";
 
 export default combineReducers({
     errors:errorReducer,
     saveFeedBack:saveFeedbackReducer,
     recipe:recipeReducer,
     recipes:recipesReducer,
-    user:createUserReducer,
+    currentUser:currentUserReducer,
     pageTitle:pageTitleReducer,
-    categories:categoriesReducer
+    categories:categoriesReducer,
+    menus:menuReducer,
+    favoriteRecipes:favoriteRecipesReducer,
+    deleteFeedBack:deleteRecipeReducer,
+    showBackButton:backButtonReducer
 })
