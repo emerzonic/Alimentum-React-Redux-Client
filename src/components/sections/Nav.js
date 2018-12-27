2cdf2d142d42721e6ac818da92458467038599a9
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 09:09:47 -0600

    moved state to redux

--- src/components/sections/Nav.js
+++ src/components/sections/Nav.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../pages/home/home.css';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
 
-const Nav = () => (
+
+const Nav = (props) => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
             <a href="/" className="navbar-brand border-2"><i className="fas fa-utensils"></i>A</a>
@@ -14,13 +17,31 @@ const Nav = () => (
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <a href="/categories" className="nav-link">Categories</a>
-                    <a href="/user/favorites" className="nav-link">Favorites</a>
-                    <a href="/user-form/login" className="nav-link justify-self-end">Login</a>
-                    <a href="/user-form/signup" className="nav-link justify-self-end">Sign Up</a>
-                    <a href="/" className="nav-link justify-self-end">Logout</a>
+                    <a href={props.currentUser.validToken?"/user/favorites":"/user-form/login"} className="nav-link">Favorites</a>
+                    {props.currentUser.validToken?
+                    <a href="/logout" className="nav-link justify-self-end">Logout</a>
+                    :""}
+                    {!props.currentUser.validToken?
+                        <a href="/user-form/login" className="nav-link justify-self-end">Login</a>
+                    :""}
+                    {!props.currentUser.validToken?
+                        <a href="/user-form/signup" className="nav-link justify-self-end">Sign Up</a>
+                    :""}
+                    {props.currentUser.validToken?
+                        <a href="/" className="nav-link justify-self-end"><i className="fas fa-user-circle"></i> {props.currentUser.user.username}</a>
+                    :""}
+                
+                   
                 </div>
             </nav>
     </div>
 );
 
-export default Nav;
+Nav.propTypes = {
+    currentUser:PropTypes.object.isRequired
+}
+
+const mapStateToProps = state =>({
+    currentUser:state.currentUser
+})
+export default connect(mapStateToProps,null)(Nav);
