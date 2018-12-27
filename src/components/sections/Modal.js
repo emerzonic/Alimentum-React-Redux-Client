278909ae424755bb1794c8fe391ac5e90da4d3a9
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-27 09:09:41 -0600

    moved state to redux

--- src/components/sections/Modal.js
+++ src/components/sections/Modal.js
@@ -3,7 +3,8 @@ import '../pages/home/home.css';
 
 
 const Modal = (props) => (
-    <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" 
+         aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div className="modal-dialog modal-lg" role="document">
       <div className="modal-content">
         <div className="modal-header">
@@ -13,7 +14,9 @@ const Modal = (props) => (
           </button>
         </div>
         <div className="modal-body">
-            <iframe id="videoIframe" src={props.recipe.strYoutube} className="embed-responsive-item" width="100%" height="400" title="youtube" frameBorder="0" allowFullScreen>
+            <iframe id="videoIframe" src={props.recipe.strYoutube} 
+                    className="embed-responsive-item" width="100%" height="400" 
+                    title="youtube" frameBorder="0" allowFullScreen>
             </iframe>
         </div>
         <div className="modal-footer">
