f7ddfd8362456dcad42d734f41b5fe0142de6618
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-29 22:40:20 -0600

    refactor reducers and actions into separate files

---
+++ src/actions/appUtilActions.js
@@ -0,0 +1,45 @@
+import util from "../components/util"
+import assets from "../assets"
+import {
+
+    SET_PAGE_TITLE,
+    GET_CATEGORIES,
+    SET_MENUS,
+    SHOW_BACK_BUTTON,
+    
+} from "./types";
+
+//SETUP HOME PAGE MENU
+export const getMenus = () => async dispatch => {
+    let homePageMenus = util.getDataArray(assets.homeCardsObj)
+    dispatch({
+        type: SET_MENUS,
+        payload: homePageMenus
+    })
+}
+
+//GENERATE RECIPE CATEGORIES
+export const getCategories = () => async dispatch => {
+    let categories = util.getDataArray(assets.category)
+    dispatch({
+        type: GET_CATEGORIES,
+        payload: categories
+    })
+}
+
+//DYNAMIC PAGE TITLE UPDATE
+export const updatePageTitle = (pageTitle) => async dispatch => {
+    dispatch({
+        type: SET_PAGE_TITLE,
+        payload: pageTitle
+    })
+}
+
+
+//DYNAMIC DISPLAY OF BACK BUTTON
+export const updateBackButton = (status) => async dispatch => {
+    dispatch({
+        type: SHOW_BACK_BUTTON,
+        payload: status
+    })
+}
