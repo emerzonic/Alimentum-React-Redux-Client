25431b2acd854bc9da0327adf486a1b2ee0585b1
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-29 22:40:27 -0600

    refactor reducers and actions into separate files

---
+++ src/actions/exceptionAction.js
@@ -0,0 +1,28 @@
+import {
+    GET_ERRORS,RESET_ERRORS
+} from "./types";
+
+
+export const getErrors = (err, dispatch) => {   
+    if (err.response && err.response.status && err.response.status === 500) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response
+        })
+    } else {
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    }
+}
+
+
+export const resetErrors = (dispatch) => {  
+    console.log(dispatch) 
+        dispatch({
+            type: RESET_ERRORS,
+            payload: {}
+        })
+
+}
