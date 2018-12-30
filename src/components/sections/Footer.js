71d8cbe9d0ee9b7c90aaadd8d21dfe60210d4438
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-29 22:42:08 -0600

    refactor reducers and actions into separate files

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../pages/home/home.css';
 
 const Footer = () => (
-    <nav className="nav navbar-light bg-dark text-center">
+    <nav className="nav navbar-light bg-dark text-center fixed-bottom">
         <span className="item text-light p-md-5"><i className="fas fa-leaf text-success"></i><i className="fab fa-react ml-2"></i> Built with Spring Boot REST Service backend & React JS client. </span>
         <span className="item text-light p-md-5"> Copyright &copy;2018 Emerson Doyah Projects</span>
     </nav> 
