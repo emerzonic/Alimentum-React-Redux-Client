58d9fdae8aec4e8ad8da2193e42c7ea85ed3c488
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-29 22:44:42 -0600

    refactor reducers and actions into separate files

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,13 +1,36 @@
-import {GET_RECIPE_BY_ID} from "../actions/types";
+import {
+    GET_RECIPES,
+    GET_RECIPE,
+    GET_FAVORITE_RECIPES,
+    DELETE_RECIPE
+} from "../actions/types";
 
-const initialState = {}
+const initialState = {
+    recipes: [],
+    recipe: {},
+    favoriteRecipes: []
+}
 
-export default function(state=initialState, action){
+export default function (state = initialState, action) {
     switch (action.type) {
-        case GET_RECIPE_BY_ID:
-        return action.payload;    
+        case GET_RECIPES:
+            return { ...state,
+                recipes: action.payload
+            }
+        case GET_RECIPE:
+            return { ...state,
+                recipe: action.payload
+            }
+        case GET_FAVORITE_RECIPES:
+            return { ...state,
+                favoriteRecipes: action.payload
+            }
+        case DELETE_RECIPE:
+            return { ...state,
+                favoriteRecipes: state.favoriteRecipes.filter(recipe => recipe.id !== action.payload)
+            }
         default:
-            return state;
+            return state
     }
 
 }
