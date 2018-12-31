8bc5c7431ce636ef8c3d15924cd89f16282f5949
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-30 19:40:11 -0600

    move alert out of header

--- src/components/sections/Page Header.js
+++ src/components/sections/Page Header.js
@@ -8,19 +8,7 @@ const PageHeader = (props) => (
                 <div className="row">
                     <div className="col-12 mx-auto p-0">
                     <div className="rounded shadow-sm p-3 m-0 page-title-div">
-                         <h1 className="display-4 page-title mb-0">{props.pageTitle}
-                            {props.saveFeedBack && props.saveFeedBack.type==="success"? 
-                                <div className="alert alert-success save-feedback d-md-inline ml-md-5" role="alert">
-                                <i className="fas fa-check mr-2"></i>
-                                    {props.saveFeedBack.text}
-                                </div>:""
-                            } {props.saveFeedBack && props.saveFeedBack.type==="fail"? 
-                                <div className="alert alert-danger save-feedback d-md-inline ml-md-5" role="alert">
-                                <i className="fas fa-times mr-2"></i>
-                                    {props.saveFeedBack.text}
-                                </div>:""
-                            }                        
-                         </h1>
+                         <h1 className="display-4 page-title mb-0">{props.pageTitle}</h1>
                          {props.showBackButton?
                          <button className="btn btn-success mt-2 back-button px-md-3" 
                                  onClick={()=>props.history.goBack()}><i className="fas fa-backward mr-2"></i> Back</button>:""}
