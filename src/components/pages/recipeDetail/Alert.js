3ab237ad78c3829c1f7cf669633956a65804d256
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-31 10:16:39 -0600

    make back button permanent on pages

--- src/components/pages/recipeDetail/Alert.js
+++ src/components/pages/recipeDetail/Alert.js
@@ -5,27 +5,34 @@ import "./detail.css"
 
 const Alert = (props) => (
     <div>
-        {props.saveFeedBack && props.saveFeedBack.type==="success"? 
+        {props.saveFeedBack && props.saveFeedBack.type === "success"? 
             <div className="alert alert-success mb-0" role="alert">
             <i className="fas fa-check mr-2"></i>
                 {props.saveFeedBack.text}
             </div>:""
-        } {props.saveFeedBack && props.saveFeedBack.type==="fail"? 
+        } {props.saveFeedBack && props.saveFeedBack.type === "fail"? 
             <div className="alert alert-danger mb-0" role="alert">
             <i className="fas fa-times mr-2"></i>
                 {props.saveFeedBack.text}
             </div>:""
-        } 
+        } {props.deleteFeedBack.text && props.deleteFeedBack.type === "success"? 
+            <div className="alert alert-success mb-0" role="alert">
+            <i className="fas fa-check mr-2"></i>
+                {props.deleteFeedBack.text}
+            </div>:""
+        }
     </div> 
 );
 
 Alert.propTypes = {
     saveFeedBack:PropTypes.object.isRequired,
+    deleteFeedBack:PropTypes.object.isRequired,
     errors:PropTypes.object.isRequired,
 }
 
 const mapStateToProps = state =>({
     saveFeedBack:state.alerts.saveMessage,
+    deleteFeedBack:state.alerts.deleteMessage,
     errors:state.errors
 })
  
