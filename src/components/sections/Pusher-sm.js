32ea739317185a586846e6330b44a7ba9a1cc093
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-30 19:40:43 -0600

    add small pusher div to keep container above the footer

---
+++ src/components/sections/Pusher-sm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../pages/home/home.css';
+
+const PusherSmall = () => (
+   <div className="container small-pusher">
+   </div>
+);
+
+export default PusherSmall;
