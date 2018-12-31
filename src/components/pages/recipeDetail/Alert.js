9c6764550ff620b59fef62a3ae78fea6a159f6f1
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-30 19:33:42 -0600

    moved recipe detail into own functional component

---
+++ src/components/pages/recipeDetail/Alert.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import "./detail.css"
+
+const Alert = (props) => (
+    <div>
+        {props.saveFeedBack && props.saveFeedBack.type==="success"? 
+            <div className="alert alert-success mb-0" role="alert">
+            <i className="fas fa-check mr-2"></i>
+                {props.saveFeedBack.text}
+            </div>:""
+        } {props.saveFeedBack && props.saveFeedBack.type==="fail"? 
+            <div className="alert alert-danger mb-0" role="alert">
+            <i className="fas fa-times mr-2"></i>
+                {props.saveFeedBack.text}
+            </div>:""
+        } 
+    </div> 
+);
+
+Alert.propTypes = {
+    saveFeedBack:PropTypes.object.isRequired,
+    errors:PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state =>({
+    saveFeedBack:state.alerts.saveMessage,
+    errors:state.errors
+})
+ 
+export default connect(mapStateToProps, null)(Alert);
