f5685360342efa2b468f08a44778841ff56d3d7a
Emerson Doyah <emerson.doyah@gmail.com>
2018-12-31 10:17:26 -0600

    make back button permanent on pages

--- src/components/sections/Page Header.js
+++ src/components/sections/Page Header.js
@@ -7,12 +7,11 @@ const PageHeader = (props) => (
              <div className="container">
                 <div className="row">
                     <div className="col-12 mx-auto p-0">
-                    <div className="rounded shadow-sm p-3 m-0 page-title-div">
-                         <h1 className="display-4 page-title mb-0">{props.pageTitle}</h1>
-                         {props.showBackButton?
-                         <button className="btn btn-success mt-2 back-button px-md-3" 
-                                 onClick={()=>props.history.goBack()}><i className="fas fa-backward mr-2"></i> Back</button>:""}
-                    </div>
+                        <div className="rounded shadow-sm p-3 m-0 page-title-div d-flex justify-content-between">
+                            <h1 className="display-4 page-title mb-0">{props.pageTitle}</h1>
+                            <button className="btn btn-success mt-2 back-button px-md-3" 
+                                    onClick={()=>props.history.goBack()}><i className="fas fa-backward mr-2"></i> Back</button>
+                        </div>
                     </div>
                 </div>
             </div>
