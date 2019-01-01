298e324992734d1fbc51c475107dc164bc1a05e4
Emerson Doyah <emerson.doyah@gmail.com>
2019-01-01 16:53:40 -0600

    added react Link to nav

--- src/components/sections/Nav.js
+++ src/components/sections/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import '../pages/home/home.css';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
@@ -7,7 +8,7 @@ import PropTypes from "prop-types";
 const Nav = (props) => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <a href="/" className="navbar-brand border-2"><i className="fas fa-utensils"></i>A</a>
+            <Link to="/" className="navbar-brand border-2"><i className="fas fa-utensils"></i>A</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" 
                         data-target="#navbarNavDropdown" 
                         aria-controls="navbarNavDropdown" 
@@ -16,37 +17,37 @@ const Nav = (props) => (
                     <span className="text-white ml-2">Menu</span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <a href="/categories" className="nav-link">
+                    <Link to="/categories" className="nav-link">
                         <i className="fas fa-list-alt mr-2"></i>
                         Categories
-                    </a>
-                    <a href={"/user/favorites"} className="nav-link">
+                    </Link>
+                    <Link to={"/user/favorites"} className="nav-link">
                         <i className="far fa-star mr-2"></i>
                         Favorites
-                    </a>
+                    </Link>
                     {props.currentUser.validToken?
-                    <a href="/logout" className="nav-link justify-self-end">
+                    <Link to="/logout" className="nav-link justify-self-end">
                         <i className="fas fa-sign-out-alt mr-2"></i>
                             Logout
-                    </a>
+                    </Link>
                     :""}
                     {!props.currentUser.validToken?
-                        <a href="/user-form/login" className="nav-link justify-self-end">
+                        <Link to="/user-form/login" className="nav-link justify-self-end">
                             <i className="fas fa-sign-in-alt mr-2"></i>
                             Login
-                        </a>
+                        </Link>
                     :""}
                     {!props.currentUser.validToken?
-                        <a href="/user-form/signup" className="nav-link justify-self-end">
+                        <Link to="/user-form/signup" className="nav-link justify-self-end">
                             <i className="fas fa-user-plus mr-2"></i>
                             Sign Up
-                        </a>
+                        </Link>
                     :""}
                     {props.currentUser.validToken?
-                        <a href="/" className="nav-link justify-self-end">
+                        <Link to="/" className="nav-link justify-self-end">
                             <i className="fas fa-user-circle mr-2"></i> 
                             {props.currentUser.user.username}
-                        </a>
+                        </Link>
                     :""}
                 </div>
             </nav>
