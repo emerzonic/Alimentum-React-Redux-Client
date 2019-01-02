52b24b496a04bce9277e5549372f731ee9d3aa26
Emerson Doyah <emerson.doyah@gmail.com>
2019-01-02 10:31:04 -0600

    footer text edit

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -3,10 +3,9 @@ import '../pages/home/home.css';
 
 const Footer = () => (
     <div className="nav navbar-light bg-dark text-center text-light fixed-bottom">
-    <p className="footer-text m-sm-4">
-        
-        
-        Built with <i className="fas fa-leaf ml-1"></i> Spring Boot REST Service backend & <i className="fab fa-react ml-1"></i> React JS client.
+    <p className="footer-text m-sm-4">Built with 
+        <i className="fas fa-leaf ml-1"></i> Spring Boot REST Service backend & 
+        <i className="fab fa-react ml-1"></i> React/Redux client.
          Copyright &copy;2018 Emerson Doyah Projects
     </p>
     </div> 
